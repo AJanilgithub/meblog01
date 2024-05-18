9adcdbf328681df52b14f685605c33fd8e255c8e
AJanilgithub <anilkumarjigalur86@gmail.com>
2024-05-17 23:56:10 -0700

    127:Create Save Post Feature

---
+++ src/main/java/com/meblog/Meblog01Application.java
@@ -0,0 +1,13 @@
+package com.meblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Meblog01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Meblog01Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/meblog/Meblog01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Meblog01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
